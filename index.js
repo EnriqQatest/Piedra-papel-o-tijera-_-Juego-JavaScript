24dfbba7cb492d3dc6f3c7622c418512d87f3ed5
Enrique De Los Santos <enriqtester@gmail.com>
2025-08-14 20:30:59 -0400

    Iniciando Juego

---
+++ index.js
@@ -0,0 +1,21 @@
+// Declaramos nuestras variables
+// Guardaremos en variables todos los elementos que veamos necesarios utilizar
+let botonJugar = document.querySelector(".boton-judar");
+let jugador = documento.querySelector("#emojiJugador");
+let computadora = document.querySelector("#emojiComputadora");
+let resultado = document.querySelector("#tectoResultado");
+let opciones =  document.querySelectorAll(".boton-opcion");
+
+
+
+// Ahora empezamos agregando funcionalidad cuando damos click en jugar
+
+
+
+
+// Luego creemos la funcionalidad cuando seleccionemos una opcion
+
+
+
+
+// Al final agregaremos la funcionalidad cuando la computadora piensa
